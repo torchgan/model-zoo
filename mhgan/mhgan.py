53800d107150fe0d20b1dd079755b1f0240715bf
k-k-d <krishnadhakshin2000@gmail.com>
2019-06-11 00:43:44 +0530

    isort and yapf done readme updated

--- mhgan/mhgan.py
+++ mhgan/mhgan.py
@@ -1,33 +1,45 @@
 import os
 import random
-import matplotlib.pyplot as plt
+
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 import numpy as np
-from IPython.display import HTML
 import torch
 import torch.nn as nn
-import torchvision
-from torch.optim import Adam
-from torch.optim import SGD
-import torch.nn as nn
 import torch.utils.data as data
+import torchgan
+import torchvision
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
-import torchgan
-from torchgan.models import *
+from IPython.display import HTML
+from torch.optim import SGD, Adam
 from torchgan.losses import *
+from torchgan.models import *
 from torchgan.trainer import Trainer
 
-dataset = dsets.MNIST(root='./mnist', train=True, transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(), transforms.Normalize(mean = (0.5,), std = (0.5,))]), download=True)
+dataset = dsets.MNIST(root='./mnist',
+                      train=True,
+                      transform=transforms.Compose([
+                          transforms.Resize((32, 32)),
+                          transforms.ToTensor(),
+                          transforms.Normalize(mean=(0.5, ), std=(0.5, ))
+                      ]),
+                      download=True)
 
-dataloader = data.DataLoader(dataset, batch_size=64, shuffle=True, num_workers=2)
+dataloader = data.DataLoader(dataset,
+                             batch_size=64,
+                             shuffle=True,
+                             num_workers=2)
 
 real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(8, 8))
 plt.axis("off")
 plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0][:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.imshow(
+    np.transpose(
+        vutils.make_grid(real_batch[0][:64], padding=2, normalize=True).cpu(),
+        (1, 2, 0)))
 plt.show()
 
 acgan = {
@@ -38,7 +50,7 @@ acgan = {
             "num_classes": 10,
             "out_channels": 1,
             "step_channels": 32,
-            "out_size":32,
+            "out_size": 32,
             "nonlinearity": nn.LeakyReLU(0.2),
             "last_nonlinearity": nn.Tanh()
         },
@@ -70,7 +82,12 @@ acgan = {
     }
 }
 
-loss = [MinimaxDiscriminatorLoss(), MinimaxGeneratorLoss(), AuxiliaryClassifierGeneratorLoss(), AuxiliaryClassifierDiscriminatorLoss(),]
+loss = [
+    MinimaxDiscriminatorLoss(),
+    MinimaxGeneratorLoss(),
+    AuxiliaryClassifierGeneratorLoss(),
+    AuxiliaryClassifierDiscriminatorLoss(),
+]
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -87,10 +104,16 @@ trainer = Trainer(acgan, loss, sample_size=64, epochs=epochs, device=device)
 
 trainer(dataloader)
 
-fig = plt.figure(figsize=(8,8))
+fig = plt.figure(figsize=(8, 8))
 plt.axis("off")
-ims = [[plt.imshow(plt.imread("{}/epoch{}_generator.png".format(trainer.recon, i)))] for i in range(1, trainer.epochs + 1)]
-ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+ims = [[
+    plt.imshow(plt.imread("{}/epoch{}_generator.png".format(trainer.recon, i)))
+] for i in range(1, trainer.epochs + 1)]
+ani = animation.ArtistAnimation(fig,
+                                ims,
+                                interval=1000,
+                                repeat_delay=1000,
+                                blit=True)
 HTML(ani.to_jshtml())
 
 gen = trainer.generator
@@ -98,17 +121,17 @@ gen = trainer.generator
 dis = trainer.discriminator
 
 for i in range(10):
-  x = torch.randn([1,100], device=device)
-  for k in range(1000):
-    xk = torch.randn([1,100], device=device)
-    a = 1/dis(gen(x, torch.Tensor([i]).cuda()))
-    b = 1/dis(gen(xk, torch.Tensor([i]).cuda()))
-    d = (a-1)/(b-1)
-    p = torch.rand([1,1], device=device)
-    if (p < min(1, d)):
-      x = xk
-  image = gen(x, torch.Tensor([i]).cuda())
-  plt.figure()
-  plt.axis("off")
-  plt.title(i)
-  plt.imshow(np.transpose(vutils.make_grid(image.detach()).cpu(),(1,2,0)))
+    x = torch.randn([1, 100], device=device)
+    for k in range(1000):
+        xk = torch.randn([1, 100], device=device)
+        a = 1 / dis(gen(x, torch.Tensor([i]).cuda()))
+        b = 1 / dis(gen(xk, torch.Tensor([i]).cuda()))
+        d = (a - 1) / (b - 1)
+        p = torch.rand([1, 1], device=device)
+        if (p < min(1, d)):
+            x = xk
+    image = gen(x, torch.Tensor([i]).cuda())
+    plt.figure()
+    plt.axis("off")
+    plt.title(i)
+    plt.imshow(np.transpose(vutils.make_grid(image.detach()).cpu(), (1, 2, 0)))
